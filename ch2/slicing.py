d8677a1c343d2f6eff18ab8c7e931d84db8ccbb1
Francesco Piscani <stem.apks@gmail.com>
2015-01-13 12:29:26 -0500

    Create slicing.py

---
+++ ch2/slicing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import cv2 
+
+image = cv2.imread("newport.png") 
+cv2.imshow("original", image) 
+
+
+sliced = image[240:380, 550:742] 
+
+cv2.imshow("Sliced", sliced) 
+cv2.waitKey(0) 
+
