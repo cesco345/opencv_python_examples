cb6745e9825c6589b251d71fc0a461a61c1a60d3
Francesco Piscani <stem.apks@gmail.com>
2015-07-25 10:19:20 -0400

    Create calculate_probability.py

---
+++ ch6/calculate_probability.py
@@ -0,0 +1,65 @@
+import csv
+import random
+import math
+
+def loadCsv(filename):
+	lines = csv.reader(open(filename, "rb"))
+	dataset = list(lines)
+	for i in range(len(dataset)):
+		dataset[i] = [float(x) for x in dataset[i]]
+	return dataset
+
+def splitDataset(dataset, splitRatio):
+	trainSize = int(len(dataset) * splitRatio)
+	trainSet = []
+	copy = list(dataset)
+	while len(trainSet) < trainSize:
+		index = random.randrange(len(copy))
+		trainSet.append(copy.pop(index))
+	return [trainSet, copy]
+
+def separateByClass(dataset):
+	separated = {}
+	for i in range(len(dataset)):
+		vector = dataset[i]
+		if (vector[-1] not in separated):
+			separated[vector[-1]] = []
+		separated[vector[-1]].append(vector)
+	return separated
+
+def mean(numbers):
+        return sum(numbers)/float(len(numbers))
+
+def stdev(numbers):
+        avg = mean(numbers)
+        variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+        return math.sqrt(variance)
+
+def summarize(dataset):
+	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+	del summaries[-1]
+	return summaries
+
+def summarizeByClass(dataset):
+	separated = separateByClass(dataset)
+	summaries = {}
+	for classValue, instances in separated.iteritems():
+		summaries[classValue] = summarize(instances)
+	return summaries
+
+def calculateProbability(x, mean, stdev):
+	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+
+
+
+def main():
+	filename = 'pima_diabetes.csv'
+	dataset = loadCsv(filename)
+
+	x = 71.5
+	mean = 73
+	stdev = 6.2
+	probability = calculateProbability(x, mean, stdev)
+	print('Probability of belonging to this class: {0}').format(probability)
+main()
